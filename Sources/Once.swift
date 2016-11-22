1e4f491c01a7bf96b61348035b91ff69983b943f
noppoman <yuki@miketokyo.com>
2016-11-23 05:28:18 +0900

    add Once

---
+++ Sources/Once.swift
@@ -0,0 +1,27 @@
+//
+//  Once.swift
+//  Prorsum
+//
+//  Created by Yuki Takei on 2016/11/23.
+//
+//
+
+import Foundation
+
+public class Once {
+    fileprivate let cond = Cond()
+    
+    private var done = false
+    
+    public func `do`(_ task: @escaping (Void) -> Void){
+        cond.mutex.lock()
+        defer {
+            cond.mutex.unlock()
+        }
+        
+        if !done {
+            done = true
+            task()
+        }
+    }
+}

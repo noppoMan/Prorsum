d74797c331937ea589d759c020f80b607a4dc839
井川司 <tsukasa.ikawa@secual-inc.com>
2019-09-10 17:08:01 +0900

    correspond to swift5.0

--- Package@swift-4.0.swift
+++ Package@swift-4.0.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "Prorsum", targets: ["Prorsum"])
     ],
     dependencies: [
-        .package(url: "https://github.com/tuken/CHTTPParser.git", from: "0.15.0"),
-        .package(url: "https://github.com/tuken/ProrsumNet.git", from: "0.3.0"),
+        .package(url: "https://github.com/Zewo/CHTTPParser.git", .exact("0.14.0")),
+        .package(url: "https://github.com/tuken/ProrsumNet.git", from: "0.2.0"),
     ],
     targets: [
         .target(name: "Prorsum", dependencies: ["CHTTPParser", "ProrsumNet"]),
